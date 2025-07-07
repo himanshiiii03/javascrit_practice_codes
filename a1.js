7132342027db6223ff138d8be41f923fb13b23be
unknown <dubeyhimanshi347@gmail.com>
2025-07-07 18:06:11 +0200

    this is my 2nd commit

---
+++ a1.js
@@ -0,0 +1,14 @@
+ const accountId = 12345
+ let accountEmail = "himanshi@google.com"
+ var accountPassword = "112233"
+ accountCity = "Jabalpur"
+ 
+ //accountId = 2
+
+ accountEmail = "dubey@gmail.com"
+ accountPassword = "21211"
+ accountCity = " mandla"
+
+ console.log(accountId);
+
+ console.table([accountId,accountEmail,accountPassword,accountCity]);
