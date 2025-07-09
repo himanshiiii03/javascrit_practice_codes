e56d414b028c5ffc61cdba76dec94e73772e1428
unknown <dubeyhimanshi347@gmail.com>
2025-07-09 19:32:41 +0530

    this is my todays commit

---
+++ a2.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser 
+
+console.log(3
+    +
+     3) //code redability that should be high
+
+console.log('himanshi')
+
+
+let name = "himanshi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number =>2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+//null => standlone value
+// undefined =>
+//symbol => unique
+
+
+// object
+
+console.log(typeof null); //object
+console.log(type undefined); //undefined
