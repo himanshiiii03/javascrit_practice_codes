dcc3b46af7159a3e32154cbdfaeb8b7ebd58e417
unknown <dubeyhimanshi347@gmail.com>
2025-07-12 00:03:37 +0530

    this is my commit

---
+++ a5.js
@@ -0,0 +1,37 @@
+//prmitive 
+
+// 7 type : string ,number, boolean, null, undefined,symbol,Bigint
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let usserEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const BigNumber = 3445789345222n
+
+
+
+
+// //refrence(non primitive)
+
+// //array,objects,function
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "himanshi",
+//     age: 21,
+// }
+
+//  const myFunction = function(){
+//     console.log("hello world");
+//  }
+
+//  console.log(typeOf (myFunction));
+
