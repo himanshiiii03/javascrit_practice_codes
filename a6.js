aae916650ba607ff38ed4ad9d89040560156ac1d
unknown <dubeyhimanshi347@gmail.com>
2025-07-13 00:28:20 +0530

    this id string releted commit

---
+++ a6.js
@@ -0,0 +1,19 @@
+const name = "himanshi"
+const repoCount = 50
+
+//console.log(name + repoCount+ "value");
+
+console.log(`hello my name is ${name} and my repo count is $ {repoCount}`);
+
+const gameName =  new String('himanshidu')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+
