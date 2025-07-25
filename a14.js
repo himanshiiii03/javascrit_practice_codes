c2e0b72d4bb678a912d65b6f2891e77895f98e70
unknown <dubeyhimanshi347@gmail.com>
2025-07-25 14:54:26 +0530

    this is IIFE

---
+++ a14.js
@@ -0,0 +1,11 @@
+// immediately invoked function expressions (IIFE)
+
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+}) (`himanshi`)
+
